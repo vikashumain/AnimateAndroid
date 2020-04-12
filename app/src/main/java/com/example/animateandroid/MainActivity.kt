a5d3139ac8c69f26469888203063ec91c7cf8781
vikas.bajpayee <vikas.bajpayee@aux.gojek.com>
2020-04-12 19:56:02 +0530

    Add layout file with buttons and image object

---
+++ app/src/main/java/com/example/animateandroid/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.animateandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button1 = findViewById<Button>(R.id.button)
+        val button2 = findViewById<Button>(R.id.button2)
+        val button3 = findViewById<Button>(R.id.button3)
+        val button4 = findViewById<Button>(R.id.button4)
+        val button5 = findViewById<Button>(R.id.button5)
+
+    }
+}
